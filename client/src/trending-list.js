159fb750b8b443685d7fef2fca39e396307c5a25
Onesun15 <sunnysharma15@hotmail.com>
2017-11-26 20:03:24 -0500

    added css file for trending-list

--- client/src/trending-list.js
+++ client/src/trending-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchTrending } from './actions/index'
+import './trending-list.css';
 
 
 class TrendingList extends Component {
